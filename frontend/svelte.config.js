ba87e4c2878db6bcb00310744f08ef950ce5303e
hadi499 <hadi.ygcl@gmail.com>
2026-06-24 09:13:57 +0700

    fix adapter issue and add notes feature

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,6 +3,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+compilerOptions: {
+  runes: true, // Will test if this breaks anything. Wait, svelte.config.js doesn't allow functions? Actually it does.
+},
 preprocess: vitePreprocess(),
 kit: {
 adapter: adapter()
